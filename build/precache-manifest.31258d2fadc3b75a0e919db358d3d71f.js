b6f2e997dcb153ed197631fa61bffb0c9bfc7c17
aobeidh <amerobeidah@gmail.com>
2020-06-17 14:46:13 +0300

    Initial Commit

---
+++ build/precache-manifest.31258d2fadc3b75a0e919db358d3d71f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8523dce6d386b7ad81f3940fb1fc027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7cc89de86bd7fd3b0fe",
+    "url": "/static/css/main.e471962c.chunk.css"
+  },
+  {
+    "revision": "a5722feb0b77e11c19b7",
+    "url": "/static/js/2.780b7203.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.780b7203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cc89de86bd7fd3b0fe",
+    "url": "/static/js/main.2dfa0f34.chunk.js"
+  },
+  {
+    "revision": "8fe1b2a24688a03304bc",
+    "url": "/static/js/runtime-main.156c33a4.js"
+  },
+  {
+    "revision": "095fc4ba7e3300810adff6147becd322",
+    "url": "/static/media/NizarCoconKurdishBold.095fc4ba.otf"
+  },
+  {
+    "revision": "f1c9ef93b6e3f83f3308be5f273e3b46",
+    "url": "/static/media/footer-background.f1c9ef93.png"
+  }
+]);
